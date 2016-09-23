90e069ec1a608338d0280c54b79b9ff3933eeff5
Nick Moreton <nickmoreton@MacBook-Pro.local>
2016-09-23 16:17:42 +0100

    Initial commit

---
+++ readme.md
@@ -0,0 +1,37 @@
+# Simple RSS JS Plugin
+
+## Usage
+
+Add the plugin to your page, just before the `</body>` tag.
+
+`<script src="path/to/simple-rss.js"></script>`
+
+Create the container for the RSS feed to show in
+
+```
+<div data-rss-feed="http://answermethis.libsyn.com/rss" data-rss-link-titles="false" data-rss-title-wrapper="h3" data-rss-max="5"></div>
+```
+
+### Options
+
+#### data-rss-feed (required)
+
+Specifies the RSS feed to pull content from
+
+#### data-rss-link-titles (optional)
+
+Specifies whether to link the titles to the original post.
+
+_default: true_
+
+#### data-rss-title-wrapper (optional)
+
+Specifies the HTML element to wrap the titles.
+
+_default: h2_
+
+#### data-rss-max (optional)
+
+Specifies the number of entries to output
+
+_default: 10_
---
+++ simple-rss.js
@@ -0,0 +1,79 @@
+var simpleRSSPlugin = (function() {
+  
+  // get all the feed containers
+  var feedsNodes = document.querySelectorAll('[data-rss-feed]');
+  
+  // Convert to array
+  var feeds = [].slice.call(feedsNodes);
+
+  for (var i = 0; i < feeds.length; i++) {
+
+    var container = feedsNodes[i];
+    
+    // get feed URL
+    var url = container.getAttribute('data-rss-feed');
+
+    // get whether to link titles
+    var addLink = container.getAttribute('data-rss-link-titles') || 'true';
+    
+    // get title wrapper element
+    var titleWrapper = container.getAttribute('data-rss-title-wrapper') || 'h2';
+  
+    // Max outputs
+    var max = container.getAttribute('data-rss-max') || 10;
+
+    // Get data - append as script with callback to avoid CORS
+    var script = document.createElement('script');
+
+    script.src = document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&output=json_xml&num=10&callback=simpleRSSPlugin.handleJSON&q=' + encodeURIComponent(url);
+
+    document.querySelector('head').appendChild(script);
+    
+    // Remove script
+    script.parentNode.removeChild(script);
+
+  }
+
+  // Callback function
+  function handleJSON(data) {
+
+    if (data.responseData.feed && data.responseData.feed.entries) {
+      
+      var docFrag = document.createDocumentFragment();
+
+      for (var i = 0; i < data.responseData.feed.entries.length; i++) {
+
+        var e = data.responseData.feed.entries[i];
+
+        var tempNode = document.createElement('div');
+
+        var template = '<' + titleWrapper + '><a href="' + e.link + '">' + e.title + '</a></' + titleWrapper + '>' + e.content;
+
+        if (addLink === 'false') {
+
+          template = '<' + titleWrapper + '>' + e.title + '</' + titleWrapper + '>' + e.content;
+
+        }
+
+        if (i < max) {
+          
+          tempNode.innerHTML = template;
+          
+          docFrag.appendChild(tempNode);
+
+        }
+
+      }
+      
+      container.appendChild(docFrag);
+
+    }
+  }
+  
+  // Return function for use in global scope
+  
+  return {
+    handleJSON:handleJSON
+  }
+
+})();
